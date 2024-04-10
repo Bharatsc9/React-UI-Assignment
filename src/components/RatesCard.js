3bced5f46ab093150d0be64ad58ecd4689212d34
bharatsc9 <bharatsc9@gmail.com>
2024-04-10 21:34:33 +0530

    Icons

--- src/components/RatesCard.js
+++ src/components/RatesCard.js
@@ -2,54 +2,89 @@ import React from "react";
 
 const MockData = [
   {
+    image: "https://ik.imagekit.io/oog7a5ln3/BHARAT/CATALIX%20ASSESMENT/image%201.png?updatedAt=1712477476961",
     title: "EFFICIENCY",
     content: "Lorem Ipsum",
-    value: [{ val: "1.1321" }, { icon: "Link" }],
-    price: "+0.0018 (+0.18%)",
-    rate: "0.00 USD",
+    value: "1.1321",
+    icon: "./photos/arrow-down.png",
+    positive: true,
+    price: "0.0015",
+    percentageChange: "0.18%",
+    currencyType: "USD",
+    currencyAmount: "0.00",
   },
   {
-    title: "EFFICIENCY",
+    title: "TIME TO MARKET",
     content: "Lorem Ipsum",
-    value: [{ val: "1.1321" }, { icon: "Link" }],
-    price: "+0.0018 (+0.18%)",
-    rate: "0.00 USD",
+    value: "627,176",
+    icon: "./photos/Arrow Up.png",
+    positive: true,
+    price: "97,914.00",
+    percentageChange: "26.06%",
+    currencyType: "JPY",
+    currencyAmount: "161,940,726,108",
   },
   {
-    title: "EFFICIENCY",
+    title: "TECH DEBT",
     content: "Lorem Ipsum",
-    value: [{ val: "1.1321" }, { icon: "Link" }],
-    price: "+0.0018 (+0.18%)",
-    rate: "0.00 USD",
+    value: "1.1763",
+    icon: "./photos/Arrow Up.png",
+    positive: true,
+    price: "0.0015",
+    percentageChange: "0.13%",
+    currencyType: "USD",
+    currencyAmount: "0.00",
   },
   {
-    title: "EFFICIENCY",
+    title: "PREDICTABILITY",
     content: "Lorem Ipsum",
-    value: [{ val: "1.1321" }, { icon: "Link" }],
-    price: "+0.0018 (+0.18%)",
-    rate: "0.00 USD",
+    value: "0.00616010",
+    icon: "./photos/arrow-down.png",
+    positive: true,
+    price: "0.00050460",
+    percentageChange: "16.88%",
+    currencyType: "BTC",
+    currencyAmount: "1,566",
   },
 ];
 
 const RatesCard = () => {
   return (
-    <div>
+    <div className="main-card">
       {MockData.map((cardItem, index) => {
-        const { title, content, value, price, rate } = cardItem;
+        const {
+          image,
+          title,
+          content,
+          value,
+          icon,
+          positive,
+          price,
+          percentageChange,
+          currencyType,
+          currencyAmount,
+        } = cardItem;
         return (
-          <div key={index}>
-            <div>{title}</div>
-            <div>{content}</div>
-            <div>
-              {value.map((valueItem, idx) => (
-                <div key={idx}>
-                  <div>{valueItem.val}</div>
-                  <div>{valueItem.icon}</div>
-                </div>
-              ))}
+          <div style={{backgroundImage: `url(${image})`}} className="small-card" key={index}>
+            <div className="bold-title">{title}</div>
+            <div className="light-title">{content}</div>
+            <div className="card-num">
+              <div>
+                {value} 
+              </div>
+              <div>
+              <img className="drop-down" src={icon}/> 
+              </div>
             </div>
-            <div>{price}</div>
-            <div>{rate}</div>
+            <div className="price-card">
+              <div>
+                {positive ? "+" : "-"} {price}
+              </div>
+              <div className="cent-card">
+                ({positive ? "+" : "-"} {percentageChange})
+              </div>
+            </div>
+            <div className="currency">{`${currencyAmount} ${currencyType}`}</div>
           </div>
         );
       })}
